d82dceb3eff3d0b964186a37a1c14a045b4aff4b
Freddy Aguilar <faa88820@gmail.com>
2021-05-22 23:47:00 -0700

    User rating systems

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 const passportLocalMongoose = require("passport-local-mongoose");
 
 const accountSchema = new mongoose.Schema({
-    username: {type: String}
+    username: {type: String},
+    rating: {type:Number, default: 0}
 });
 
 accountSchema.plugin(passportLocalMongoose,);
