525fa3d731100136115aefa0d59610b46d062238
Ruben De La Torre <rubenbdlt@g.ucla.edu>
2021-05-25 21:50:38 -0700

    Account Creation Skeleton and FAQ

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,16 @@
 // client/src/App.js
 
 import React from "react";
-import logo from './logo.svg';
-import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import CreateAcc from "./components/CreateAcc";
+import FAQ from "./components/FAQ";
 
 function App() {
-    const [data, setData] = React.useState(null);
-
-    React.useEffect(() => {
-	fetch("/api")
-	    .then((res) => res.json())
-	    .then((data) => setData(data.message));
-    }, []);
-
-    return (
-	<div className="App">
-	    <header className="App-header">
-		<img src={logo} className="App-logo" alt="logo" />
-		<p>{!data ? "Loading..." : data}</p>
-	    </header>
-	</div>
-    );
+  return (
+	  <div className="App">
+	  <CreateAcc/>
+	  </div>
+  );
 }
 
 export default App;
