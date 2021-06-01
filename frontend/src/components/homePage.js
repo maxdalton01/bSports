f677ee7a2c8e2c69a4c3c8e186ca16ea3281eada
Shayan Ghafoori <shayan@vpn-172-27-174-70.host.ucla.edu>
2021-06-01 04:37:47 -0700

    clear homepage and post design + better display of total atendees

--- frontend/src/components/homePage.js
+++ frontend/src/components/homePage.js
@@ -78,7 +78,7 @@ class HomePage extends React.Component {
                 .then(response => {
                     const allEvents = response.data;
                     this.setState({allEvents: allEvents, sportFilter: null, locationFilter: null, submitterValue:"CLEAR FILTER ✖",
-                        submitterBackground: 'white', submitterColor: "black"});
+                        submitterBackground: '#D3D3D3', submitterColor: "black"});
                 })
         }
         catch(err)
@@ -127,10 +127,10 @@ class HomePage extends React.Component {
                     <div style={{padding: '16px'}}>
 
                         { this.state.allEvents.reverse().map(post=> <ul class ="posts">
-                            <div className="creatorTitle">u/{post.creator} is looking for {post.wantedAttendees} attendees </div>
+                            <div className="creatorTitle">Posted by u/{post.creator} </div>
                             <h1 class ="postTitle"> {" "}{post.sport} {" "} @
                        {post.location}  </h1>
-                           <h1 className={"date"}> {date.format(new Date(post.date), 'ddd hh:mm A, MMM DD YYYY')}</h1> <h1 style={{float: "top"}}> <ul className={"attendees"}> Attendees: {post.attendees} <ul>{'----------------'}<ul>
+                           <h1 className={"date"}> {date.format(new Date(post.date), 'ddd hh:mm A, MMM DD YYYY')}</h1> <h1 style={{float: "top"}}> <ul className={"attendees"}> Attendees: {post.attendees}/{post.wantedAttendees} <ul>{'----------------'}<ul>
                             </ul><button id = {post._id} className='rsvpButton' onClick={this.handleRSVP}> RSVP</button>
                            </ul>
                            </ul> </h1> <hr/> <h3 className={"description"}> {post.description} </h3>
